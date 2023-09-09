9cff5a6c631138d3f2a79f97f861abfe7a26651c
Rach Pradhan <54503978+justrach@users.noreply.github.com>
2023-09-09 13:21:46 -0700

    added new files

--- script.py
+++ script.py
@@ -1,26 +1,16 @@
 import replicate
-import os
+output = replicate.run(
+ "jagilley/controlnet-depth2img:922c7bb67b87ec32cbc2fd11b1d5f94f0ba4f5519c4dbd02856376444127cc60",
+input={"strength":0.9,"a_prompt":"Best quality, extremely detailed","structure":"lineart","image": open("/Users/rachpradhan/Desktop/condensation/00001.png", "rb"),"prompt":"Photo of a sci-fi handsome guy with silver hair, photorealistic, in the style of Franciszek Starowieyski. He has a white porcelain sci-fi appearance with machine aesthetics reminiscent of mecha designs. The image should be in 32k UHD resolution, with a color palette dominated by dark white, azure, silver, and hints of pink. The background should depict a science fiction city. His hair should have a shiny silver appearance, and the artwork should capture him from the waist up, resembling an oil painting in the manner of Hans Zatzka."}
+)
+print(output)
+
+# Download the link from output[1] and save it to the computer
+link = output[1]
+# Code to download the link and save it to the computer goes here
 import urllib.request
 
-input_folder = "/Users/rachpradhan/Desktop/condensation/input_images"
-output_folder = "/Users/rachpradhan/Desktop/condensation/output_links"
+image_path = "/Users/rachpradhan/Desktop/condensation/2.png"
+urllib.request.urlretrieve(link, image_path)
 
-input_images = []
-for filename in os.listdir(input_folder):
-    if filename.endswith(".png"):
-        input_images.append(open(os.path.join(input_folder, filename), "rb"))
 
-output_links = []
-for image in input_images:
-    output = replicate.run(
-        "jagilley/controlnet-depth2img:922c7bb67b87ec32cbc2fd11b1d5f94f0ba4f5519c4dbd02856376444127cc60",
-        input={"strength":0.9,"a_prompt":"Best quality, extremely detailed","structure":"lineart","image": image,"prompt":"impasto painting, in the style of Chaim Soutine, minimal"}
-    )
-    output_links.append(output[1])
-
-for index, link in enumerate(output_links): 
-    # Generate a new filename based on the index
-    new_filename = f"output_image_{index}.png"
-    urllib.request.urlretrieve(link, os.path.join(output_folder, new_filename))
-
-print("All images processed and links saved successfully.")
